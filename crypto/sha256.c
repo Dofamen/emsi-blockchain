fb72555e7ff135026730ebe6a168e5b6cd3abf97
Mehdi Elouakifi <elouakifi.mehdi@gmail.com>
2021-04-04 16:44:07 +0100

    update

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -3,9 +3,18 @@
 
 uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-
-	if(digest && s) return SHA256((const unsigned char *)s, len, digest);
+	SHA256_CTX sha_ctx;
 	
-return NULL;
+	if(digest && s)
+	{
+	  		
+	SHA256_Init(&sha_ctx);
+	SHA256_Update(&sha_ctx,s,len);
+	SHA256_Final(digest,&sha_ctx);
+	
+	return digest;	
+	}
 
+	return NULL;	
 }
+
