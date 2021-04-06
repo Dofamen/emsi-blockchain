d29c5220f1d7c47f64a63879a6d897a281bfc1d8
Mehdi Elouakifi <elouakifi.mehdi@gmail.com>
2021-04-06 13:16:49 +0100

    fix bugs

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,12 +1,16 @@
 #ifndef HBLK_CRYPTO_H
 #define HBLK_CRYPTO_H
 
-#include <openssl/ec.h>
-#include <openssl/evp.h>
-#include <openssl/pem.h>
-#include <openssl/sha.h>
-#include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <openssl/sha.h>
+#include <openssl/evp.h>
+#include <openssl/ec.h>
+#include <openssl/pem.h>
+#include <openssl/bio.h>
+#include <stdint.h>
+#include <sys/types.h>
 #include <sys/stat.h>
 
 #define PUB_FILENAME	"key_pub.pem"
@@ -15,6 +19,8 @@
 
 #define EC_PUB_LEN	65
 #define SIG_MAX_LEN	72
+#define EC_PRIV_LEN 100
+
 
 /**
  * struct sig_s - EC Signature structure
@@ -35,7 +41,13 @@ typedef struct sig_s
 uint8_t *sha256(int8_t const *s, size_t len,
 		uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
-
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
+EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+int ec_save(EC_KEY *key, char const *folder);
+EC_KEY *ec_load(char const *folder);
+uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg,
+				 size_t msglen, sig_t *sig);
+int ec_verify(EC_KEY const *key, uint8_t const *msg,
+			  size_t msglen, sig_t const *sig);
 
 #endif
