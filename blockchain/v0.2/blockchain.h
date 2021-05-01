ed1f7a89712b3ec92142e7bc00318b6e57bbaf46
Mehdi Elouakifi <elouakifi.mehdi@gmail.com>
2021-05-01 16:35:37 +0000

    update

--- blockchain/v0.2/blockchain.h
+++ blockchain/v0.2/blockchain.h
@@ -3,22 +3,37 @@
 
 #include "../../crypto/hblk_crypto.h"
 #include "provided/endianness.h"
-
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <llist.h>
 #include <time.h>
-
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 #include <openssl/sha.h>
 
 #define MIN(x, y) ((x) < (y) ? (x) : (y))
+
+
+#define GENESIS_BLOCK { \
+{ /* info */ \
+0 /* index */, \
+0, /* difficulty */ \
+1537578000, /* timestamp */ \
+0, /* nonce */ \
+{0} /* prev_hash */ \
+}, \
+{ /* data */ \
+"Holberton School", /* buffer */ \
+16 /* len */ \
+}, \
+"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
+"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
+}
+
 
 #define HBLK_MAGIC "HBLK"
 #define HBLK_VERSION "0.2"
@@ -93,38 +108,26 @@ typedef struct block_s
 	uint8_t	 hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
-/* GENESIS BLOCK - first block in the chain */
-#define GENESIS_BLOCK { \
-	{ /* info */ \
-		0 /* index */, \
-		0, /* difficulty */ \
-		1537578000, /* timestamp */ \
-		0, /* nonce */ \
-		{0} /* prev_hash */ \
-	}, \
-	{ /* data */ \
-		"Holberton School", /* buffer */ \
-		16 /* len */ \
-	}, \
-	"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
-	"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
-}
 
 blockchain_t *blockchain_create(void);
-block_t *block_create(block_t const *prev, int8_t const *data,
-	uint32_t data_len);
+block_t *block_create(block_t const *prev, int8_t const *data, uint32_t data_len);
+
 void block_destroy(block_t *block);
 void blockchain_destroy(blockchain_t *blockchain);
-uint8_t *block_hash(block_t const *block,
-	uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
+
+uint8_t *block_hash(block_t const *block, uint8_t hash_buf[SHA256_DIGEST_LENGTH]);
 int blockchain_serialize(blockchain_t const *blockchain, char const *path);
+
 blockchain_t *blockchain_deserialize(char const *path);
 llist_t *deserialize_blocks(int fd, uint32_t size, uint8_t endianness);
-int block_is_valid(block_t const *block, block_t const *prev_block);
 
-int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH],
-	uint32_t difficulty);
+int block_is_valid(block_t const *block, block_t const *prev_block);
+int hash_matches_difficulty(uint8_t const hash[SHA256_DIGEST_LENGTH], uint32_t difficulty);
+
 void block_mine(block_t *block);
 uint32_t blockchain_difficulty(blockchain_t const *blockchain);
+
+uint8_t _get_endianness(void);
+void _swap_endian(void *p, size_t size);
 
 #endif
